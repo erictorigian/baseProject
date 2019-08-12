b19adec203a015e37ed08dc35b38534ee6d63567
Eric Torigian <eric@torigian.com>
2019-08-12 13:08:44 -0400

    UI Ready for firebase connections

--- baseProject/ViewControllers/LoginViewController.swift
+++ baseProject/ViewControllers/LoginViewController.swift
@@ -10,21 +10,25 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupElements()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupElements() {
+        self.errorLabel.alpha = 0
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passwordTextField)
+        Utilities.styleFilledButton(loginButton)
     }
-    */
-
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    }
+    
 }
--- baseProject/ViewControllers/ViewController.swift
+++ baseProject/ViewControllers/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupElements()
+
+    }
+    
+    func setupElements() {
+        Utilities.styleHollowButton(signupButton)
+        Utilities.styleFilledButton(loginButton)
     }
 
 
