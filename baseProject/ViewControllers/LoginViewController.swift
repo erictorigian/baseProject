4d74183909b9f2dd3555ee9def371495393ca245
Eric Torigian <eric@torigian.com>
2019-08-13 13:22:26 -0400

    login working

--- baseProject/ViewControllers/LoginViewController.swift
+++ baseProject/ViewControllers/LoginViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import FirebaseAuth
+
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var errorLabel: UILabel!
@@ -17,7 +19,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupElements()
     }
     
@@ -28,7 +30,55 @@ class LoginViewController: UIViewController {
         Utilities.styleFilledButton(loginButton)
     }
     
-    @IBAction func loginButtonTapped(_ sender: Any) {
+    func validateFields() -> String? {
+        //check if fields are populated and password is valid.  if all ok, return nil otherwise error message as string
+        
+        //check all fields are populatd
+        if self.passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            self.emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return "Please fill in all fields"
+        }
+        
+        return nil
     }
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        
+        //validate text fields
+        let error = validateFields()
+        if error != nil {
+            //error on the form
+            showError(error!)
+        } else {
+            //sign in the user
+            let email = self.emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+                       let password = self.passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            Auth.auth().signIn(withEmail: email, password: password) { (result, err) in
+                if err != nil {
+                    self.showError(err!.localizedDescription)
+                } else {
+                    self.transitionToHome()
+                }
+            }
+            
+        }
+        
+        
+        //transition to home scrren
+    }
+    
+    func showError(_ message:String) {
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
+    
+    func transitionToHome() {
+        let homeViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+        
+        view.window?.rootViewController = homeViewController
+        view.window?.makeKeyAndVisible()
+    }
+    
     
 }
